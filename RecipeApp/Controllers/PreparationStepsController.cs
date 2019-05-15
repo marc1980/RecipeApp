95b463889376df9f91180a90990aede1b1e3e34b
Marc <mph80@hotmail.com>
2019-05-15 16:34:56 +0200

    add preparation step form

--- RecipeApp/Controllers/PreparationStepsController.cs
+++ RecipeApp/Controllers/PreparationStepsController.cs
@@ -25,7 +25,8 @@ namespace RecipeApp.Controllers
         {
             var PreparationStepViewModel = new PreparationStepViewModel();
             PreparationStepViewModel.PreparationSteps = await _context.PreparationSteps
-                    .Where( i => i.RecipeId == recipeId)
+                    .Where( p => p.RecipeId == recipeId)
+                    .OrderBy(p => p.Rank)
                     .ToListAsync();
             PreparationStepViewModel.NewStep = new PreparationStep() { RecipeId = recipeId };
             return View("Change",PreparationStepViewModel);
@@ -42,6 +43,11 @@ namespace RecipeApp.Controllers
             if (ModelState.IsValid)
             {
                 var recipeId = PreparationStepViewModel.NewStep.RecipeId;
+                int highestRank = _context.PreparationSteps
+                    .Where(p => p.RecipeId == recipeId)
+                    .Max(p => p.Rank);
+
+                PreparationStepViewModel.NewStep.Rank = ++highestRank;
 
                 _context.Add(PreparationStepViewModel.NewStep);
                 await _context.SaveChangesAsync();
@@ -58,6 +64,60 @@ namespace RecipeApp.Controllers
         {
             var PreparationStepToDelete = await _context.PreparationSteps.FindAsync(id);
             _context.PreparationSteps.Remove(PreparationStepToDelete);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("List",new { recipeId = recipeId });
+        }
+
+        // POST: PreparationSteps/MoveUp/5
+        // Move up is swapping the rank with the item above it (with lower rank)
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> MoveUp(int id, int recipeId)
+        {
+            var psToMoveUp = await _context.PreparationSteps.FindAsync(id);
+            int rankToMoveUp = psToMoveUp.Rank;
+
+            var psForRecipe = await _context.PreparationSteps
+                .Where(p => p.RecipeId == recipeId)
+                .OrderByDescending(p => p.Rank)
+                .ToListAsync();
+            
+            var psToSwap = psForRecipe
+                .SkipWhile(p => p.Rank >= rankToMoveUp)
+                .FirstOrDefault();
+
+            psToMoveUp.Rank = psToSwap.Rank;
+            psToSwap.Rank = rankToMoveUp;
+            
+            _context.PreparationSteps.Update(psToMoveUp);
+            _context.PreparationSteps.Update(psToSwap);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("List",new { recipeId = recipeId });
+        }
+
+        // POST: PreparationSteps/MoveUp/5
+        // Move up is swapping the rank with the item above it (with lower rank)
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> MoveDown(int id, int recipeId)
+        {
+            var psToMoveDown = await _context.PreparationSteps.FindAsync(id);
+            int rankToMoveDown = psToMoveDown.Rank;
+
+            var psForRecipe = await _context.PreparationSteps
+                .Where(p => p.RecipeId == recipeId)
+                .OrderBy(p => p.Rank)
+                .ToListAsync();
+            
+            var psToSwap = psForRecipe
+                .SkipWhile(p => p.Rank <= rankToMoveDown)
+                .FirstOrDefault();
+
+            psToMoveDown.Rank = psToSwap.Rank;
+            psToSwap.Rank = rankToMoveDown;
+            
+            _context.PreparationSteps.Update(psToMoveDown);
+            _context.PreparationSteps.Update(psToSwap);
             await _context.SaveChangesAsync();
 
             return RedirectToAction("List",new { recipeId = recipeId });
